b2110069e768862318ae45c9e81e59d93f75f171
Mainendra <1897340+mainendra@users.noreply.github.com>
2022-09-26 11:57:25 -0400

    persistant storage

--- webserver.ts
+++ webserver.ts
@@ -1,15 +1,8 @@
 import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 import cors from "https://deno.land/x/edge_cors/src/cors.ts";
 
-let profile = {};
-
-serve(req => {
-    if (req.method === 'POST') {
-        return req.json().then(data => {
-            profile = data;
-            return Response.json(profile);
-        });
-    } else {
-        return cors(req, Response.json(profile));
-    }
+serve(async req => {
+    const request = await fetch('https://api.npoint.io/8be2c43656eaf1235cd1');
+    const profile = await request.json();
+    return cors(req, Response.json(profile));
 });
